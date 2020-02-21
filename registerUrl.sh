896006769ee221b6fbadb9d715921a7684478f1a
Jeff <jeff840107@gmail.com>
2020-02-20 18:26:25 -0600

    update README

--- registerUrl.sh
+++ registerUrl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$1" != "" ]; then
-	URL="https://le4qbchjrl.execute-api.us-east-1.amazonaws.com/test/download-page?url=" 
+	URL="https://le4qbchjrl.execute-api.us-east-1.amazonaws.com/dev/download-page?url="
 	URL+=$1
 	curl $URL
 else
